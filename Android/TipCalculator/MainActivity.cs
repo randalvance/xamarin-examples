5af1e7f017e68d220f90aae6913042193072fbb4
Randal Cunanan <randal@randalvance.net>
2017-09-13 07:44:07 +0800

    Complete TipCalculator Android

--- Android/TipCalculator/MainActivity.cs
+++ Android/TipCalculator/MainActivity.cs
@@ -7,7 +7,12 @@ namespace TipCalculator
     [Activity(Label = "TipCalculator", MainLauncher = true, Icon = "@mipmap/icon")]
     public class MainActivity : Activity
     {
-        int count = 1;
+        private EditText inputBill;
+        private Button calculateButton;
+        private TextView outputTip;
+        private TextView outputTotal;
+
+        private string amountOfBill;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -16,11 +21,24 @@ namespace TipCalculator
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.Main);
 
-            // Get our button from the layout resource,
-            // and attach an event to it
-            Button button = FindViewById<Button>(Resource.Id.myButton);
+            inputBill = FindViewById<EditText>(Resource.Id.inputBill);
+            calculateButton = FindViewById<Button>(Resource.Id.calculateButton);
+            outputTip = FindViewById<TextView>(Resource.Id.outputTip);
+            outputTotal = FindViewById<TextView>(Resource.Id.outputTotal);
 
-            button.Click += delegate { button.Text = string.Format("{0} clicks!", count++); };
+            calculateButton.Click += CalculateButtonClicked;
+        }
+
+        private void CalculateButtonClicked(object sender, System.EventArgs e)
+        {
+            amountOfBill = inputBill.Text;
+
+            double amount = double.Parse(amountOfBill);
+            double tip = amount * 0.15;
+            double total = amount + tip;
+
+            outputTip.Text = tip.ToString();
+            outputTotal.Text = total.ToString();
         }
     }
 }
