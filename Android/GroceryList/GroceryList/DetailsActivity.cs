520bdd544daa194a429861ac17a9ecba37d90d2f
Randal Cunanan <randal@randalvance.net>
2017-09-13 09:30:23 +0800

    GroceryList Android

---
+++ Android/GroceryList/GroceryList/DetailsActivity.cs
@@ -0,0 +1,25 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Widget;
+
+namespace GroceryList
+{
+	[Activity(Label = "Details")]			
+	public class DetailsActivity : Activity
+	{
+		protected override void OnCreate(Bundle bundle)
+		{
+			base.OnCreate(bundle);
+			SetContentView(Resource.Layout.Details);
+
+			int position = Intent.GetIntExtra("ItemPosition", -1);
+
+			// TODO
+
+			var item = MainActivity.Items[position];
+
+			FindViewById<TextView>(Resource.Id.nameTextView ).Text = "Name: "  + item.Name;
+			FindViewById<TextView>(Resource.Id.countTextView).Text = "Count: " + item.Count.ToString();
+		}
+	}
+}
